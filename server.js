85c61b5c13576813dfb78dd97037fb4bdb322201
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-04-23 21:51:55 -0600

    Adjust package json for Heroku

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.locals.characters = require('./allCharacters');
 app.use(cors());
 app.locals.title = 'Bard Buddy API';
 
-app.get('/allTitles', (request, response) => {
-  const allTitles = app.locals.titles;
+app.get('/allTitles/:genre', (request, response) => {
+  const { genre } = request.params
+  const allTitles = app.locals.titles.filter(play => play.genre === genre);
   if(!allTitles) {
     return response.sendStatus(404);
   }
